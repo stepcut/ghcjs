f1e160abc469625ef044cc1928ccd2ffc123a11c
Skylar-Ray O'Quinn <soraskymizu@gmail.com>
2022-06-21 22:10:04 +0000

    Add dev nix shell

---
+++ shell.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    pkgs.haskell.compiler.ghc8107
+    pkgs.cabal-install
+    pkgs.emscripten
+    pkgs.python3
+    pkgs.autoreconfHook
+    pkgs.haskellPackages.happy_1_19_12 # has to be 1.19
+    pkgs.haskellPackages.alex
+  ];
+}
+
+/*
+need
+    pkgs.haskell.compiler.ghc8107
+    pkgs.cabal-install
+    pkgs.emscripten
+    pkgs.python3
+    pkgs.autoreconfHook
+    pkgs.haskellPackages.happy_1_19_12 -- has to be happy 1.19
+
+git submodule update --init --recursive
+
+cd ghc
+If you are in a similar situation, one workaround could be to do the ./boot; ./configure in the ghc dir.
+Or you could do rm -rf ghc/.git
+
+./utils/makePackages.sh
+
+cabal v2-install --overwrite-policy=always --install-method=copy --installdir=inplace/bin
+*/
